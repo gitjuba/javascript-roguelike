1b5c0d9ee75b1cae2bf118792c0e69a58f6eab63
Jussi Mattas <jussi.mattas@gmail.com>
2021-02-14 22:24:13 +0200

    Some tilemap stuff.

--- LOG.md
+++ LOG.md
@@ -1,6 +1,21 @@
 # Development log
 
-2021-02-14
+## 2021-02-14
 
-- Started project, no idea what this will become
-- Created NPM project, added eslint and prettier
+Started project, no idea what this will become.
+
+Created NPM project, added eslint and prettier.
+
+I could make a roguelike. That's something I've always wanted to make. Or a hybrid of text adventure and tile based adventure. Or some resource management game.
+
+The starting point could anyway be a two-dimensional tile map. So make a canvas and create animation loop.
+
+Write FPS as debug text in upper left corner.
+
+Then, render a map on screen. First implementations always minimal, so write the map inline in the script file. Agree on tile dimensions (canvas width and height should be divisible).
+
+Drew tile borders using canvas moveTo and lineTo, the page became unbearably slow, started to use 100% CPU. Drawing the borders with strokeRect also uses a lot, but "only" about 50%.
+
+[Here](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Optimizing_canvas) are some useful tips to optimize `<canvas>` performance. At least the idea of drawing the background on a separate canvas seems reasonable for my use case.
+
+Before optimizing too much, let's get a player character on screen and moving. Let's force player movement to the tiles, i.e. jump from one tile to the next, no continuous motion. This is done by listening to keyup events.
--- index.js
+++ index.js
@@ -1,8 +1,90 @@
-var main = document.getElementById('main')
-main.innerHTML = 'Hello'
+var canvas = document.getElementById('main')
 
-var t = false
+var canvasWidth = 1280
+var canvasHeight = 720
 
-if (t) {
-  console.log('true')
+var tileSize = 40
+
+var tilesAcross = canvasWidth / tileSize
+var tilesDown = canvasHeight / tileSize
+
+// Initial single-screen map
+var tileMap = [
+  '#'.repeat(tilesAcross),
+  ...Array(tilesDown - 2)
+    .fill(0)
+    .map(() => '#' + '.'.repeat(tilesAcross - 2) + '#'),
+  '#'.repeat(tilesAcross),
+].map((row) => row.split(''))
+
+canvas.width = canvasWidth
+canvas.height = canvasHeight
+
+var ctx = canvas.getContext('2d')
+
+var lastTs = 0
+var dt
+var dtUpdate = 0
+var fps = 0
+
+// Initial player position
+var playerX = 3
+var playerY = 3
+
+function gameLoop(ts) {
+  dt = ts - lastTs
+  lastTs = ts
+
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight)
+
+  // Draw tile map
+  var i, j
+  for (i = 0; i < tilesDown; i++) {
+    for (j = 0; j < tilesAcross; j++) {
+      var tile = tileMap[i][j]
+      var color = 'white'
+      if (tile == '#') {
+        color = 'black'
+      }
+      ctx.fillStyle = color
+      ctx.fillRect(j * tileSize, i * tileSize, tileSize, tileSize)
+      ctx.strokeStyle = 'lightgrey'
+      ctx.strokeRect(j * tileSize, i * tileSize, tileSize, tileSize)
+    }
+  }
+
+  // Draw player
+  ctx.fillStyle = 'blue'
+  ctx.fillRect(playerX * tileSize, playerY * tileSize, tileSize, tileSize)
+
+  // Debug overlays
+  if (ts - dtUpdate > 500) {
+    fps = (1000 / dt).toFixed(1)
+    dtUpdate = ts
+  }
+  ctx.font = '10px sans-serif'
+  ctx.fillStyle = 'red'
+  ctx.fillText('FPS ' + fps, 10, 20)
+
+  window.requestAnimationFrame(gameLoop)
 }
+
+window.addEventListener('keyup', function (e) {
+  if (e.keyCode == 37) {
+    // left
+    if (tileMap[playerX - 1][playerY]) {
+    }
+  }
+  if (e.keyCode == 38) {
+    // up
+  }
+  if (e.keyCode == 39) {
+    // right
+  }
+  if (e.keyCode == 40) {
+    // down
+  }
+})
+
+window.requestAnimationFrame(gameLoop)
