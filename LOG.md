802aa990c0b8411808111e21adcddd46f9fd3eb1
Jussi Mattas <jussi.mattas@gmail.com>
2021-03-11 22:32:36 +0200

    Redraw only when changed. Separate canvases.

--- LOG.md
+++ LOG.md
@@ -19,3 +19,23 @@ Drew tile borders using canvas moveTo and lineTo, the page became unbearably slo
 [Here](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Optimizing_canvas) are some useful tips to optimize `<canvas>` performance. At least the idea of drawing the background on a separate canvas seems reasonable for my use case.
 
 Before optimizing too much, let's get a player character on screen and moving. Let's force player movement to the tiles, i.e. jump from one tile to the next, no continuous motion. This is done by listening to keyup events.
+
+## 2021-03-11
+
+Tile map is indexed "rows first", i.e. the element `tileMap[i][j]` maps to the tile on row `i`, column `j`. Contrast this with player coordinates, represented as `(x, y)`, so moving the player in the horizontal direction corresponds to changing the `j` coordinate on the tile map and vice versa.
+
+It's a bit annoying to see 50% resource consumption with a trivial _non-game_, so let's optimize anyway.
+
+Three basic methods:
+
+- Drawing only the parts of the canvas which have been changed
+- Drawing the background/scene on one canvas and the "more dynamic" entities (i.e. player, monsters, moving obstacles) on another
+- Redraw canvas only when something has changed
+
+Not sure if the first can easily be done without the second. And neither of the first two would make any sense without the third. So let's start with the second and third.
+
+To make the objects canvas transparent, it has to be cleared in the start of the frame with `ctx.clearRect()` instead of `ctx.fillRect()` with `fillStyle('white')`
+
+As simple as possible: Add a hook to the `keyup` events which toggles a flag `redrawObjects` when the player moves. Also introduce a flag `redrawLevel` which, for the moment is set to true only in the beginning.
+
+Ended up separating the debug overlay to a canvas of its own, too. Now it doesn't hog that much resources.
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var canvas = document.getElementById('main')
+var levelCanvas = document.getElementById('level')
+var objectCanvas = document.getElementById('objects')
+var overlayCanvas = document.getElementById('overlay')
 
 var canvasWidth = 1280
 var canvasHeight = 720
@@ -17,10 +19,16 @@ var tileMap = [
   '#'.repeat(tilesAcross),
 ].map((row) => row.split(''))
 
-canvas.width = canvasWidth
-canvas.height = canvasHeight
+levelCanvas.width = canvasWidth
+levelCanvas.height = canvasHeight
+objectCanvas.width = canvasWidth
+objectCanvas.height = canvasHeight
+overlayCanvas.width = canvasWidth
+overlayCanvas.height = canvasHeight
 
-var ctx = canvas.getContext('2d')
+var ctx0 = levelCanvas.getContext('2d')
+var ctx1 = objectCanvas.getContext('2d')
+var ctx2 = overlayCanvas.getContext('2d')
 
 var lastTs = 0
 var dt
@@ -31,41 +39,63 @@ var fps = 0
 var playerX = 3
 var playerY = 3
 
+// Should redraw
+var redrawLevel = true
+var redrawObjects = true
+
 function gameLoop(ts) {
   dt = ts - lastTs
   lastTs = ts
 
-  ctx.fillStyle = 'white'
-  ctx.fillRect(0, 0, canvasWidth, canvasHeight)
+  // Mostly static level map
+  if (redrawLevel) {
+    console.log('redrawing level')
+    ctx0.fillStyle = 'white'
+    ctx0.fillRect(0, 0, canvasWidth, canvasHeight)
 
-  // Draw tile map
-  var i, j
-  for (i = 0; i < tilesDown; i++) {
-    for (j = 0; j < tilesAcross; j++) {
-      var tile = tileMap[i][j]
-      var color = 'white'
-      if (tile == '#') {
-        color = 'black'
+    var i, j
+    for (i = 0; i < tilesDown; i++) {
+      for (j = 0; j < tilesAcross; j++) {
+        var tile = tileMap[i][j]
+        var color = 'white'
+        if (tile == '#') {
+          color = 'black'
+        }
+        ctx0.fillStyle = color
+        ctx0.fillRect(j * tileSize, i * tileSize, tileSize, tileSize)
+        ctx0.strokeStyle = 'lightgrey'
+        ctx0.strokeRect(j * tileSize, i * tileSize, tileSize, tileSize)
       }
-      ctx.fillStyle = color
-      ctx.fillRect(j * tileSize, i * tileSize, tileSize, tileSize)
-      ctx.strokeStyle = 'lightgrey'
-      ctx.strokeRect(j * tileSize, i * tileSize, tileSize, tileSize)
     }
+
+    redrawLevel = false
   }
 
-  // Draw player
-  ctx.fillStyle = 'blue'
-  ctx.fillRect(playerX * tileSize, playerY * tileSize, tileSize, tileSize)
+  // "Dynamic entities"
+  if (redrawObjects) {
+    console.log('redrawing objects')
+
+    ctx1.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    // Draw player
+    ctx1.fillStyle = 'blue'
+    ctx1.fillRect(playerX * tileSize, playerY * tileSize, tileSize, tileSize)
+
+    redrawObjects = false
+  }
 
   // Debug overlays
   if (ts - dtUpdate > 500) {
+    ctx2.clearRect(0, 0, canvasWidth, canvasHeight)
+
     fps = (1000 / dt).toFixed(1)
     dtUpdate = ts
+
+    // FPS
+    ctx2.font = '10px sans-serif'
+    ctx2.fillStyle = 'red'
+    ctx2.fillText('FPS ' + fps, 10, 20)
   }
-  ctx.font = '10px sans-serif'
-  ctx.fillStyle = 'red'
-  ctx.fillText('FPS ' + fps, 10, 20)
 
   window.requestAnimationFrame(gameLoop)
 }
@@ -73,17 +103,31 @@ function gameLoop(ts) {
 window.addEventListener('keyup', function (e) {
   if (e.keyCode == 37) {
     // left
-    if (tileMap[playerX - 1][playerY]) {
+    if (tileMap[playerY][playerX - 1] == '.') {
+      playerX -= 1
+      redrawObjects = true
     }
   }
   if (e.keyCode == 38) {
     // up
+    if (tileMap[playerY - 1][playerX] == '.') {
+      playerY -= 1
+      redrawObjects = true
+    }
   }
   if (e.keyCode == 39) {
     // right
+    if (tileMap[playerY][playerX + 1] == '.') {
+      playerX += 1
+      redrawObjects = true
+    }
   }
   if (e.keyCode == 40) {
     // down
+    if (tileMap[playerY + 1][playerX] == '.') {
+      playerY += 1
+      redrawObjects = true
+    }
   }
 })
 
